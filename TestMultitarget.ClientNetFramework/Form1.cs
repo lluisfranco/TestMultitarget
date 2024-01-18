065a5922f7061ffe9e0dcf5104b8ce9bce896c58
Lluis Franco <hello@lluisfranco.com>
2024-01-18 08:52:25 +0100

    Add project files.

---
+++ TestMultitarget.ClientNetFramework/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using TestMultitarget.Lib;
+
+namespace TestMultitarget.ClientNetFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += (s, e) =>
+            {
+                MessageBox.Show(new SimpleClass().Sum(2, 2).ToString());
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TestMultitarget.Lib/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMultitarget.Lib
+{
+    public class SimpleClass
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }   
+    }
+}
